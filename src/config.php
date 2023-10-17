da0865872f9e67f41a8846eb42721dbe49585892
Adria Jofre Cantón <ajofre@cendrasso.net>
2023-10-17 19:16:04 +0200

    primer commit

---
+++ src/config.php
@@ -0,0 +1,5 @@
+<?php
+
+$config = array();
+
+require_once "../src/models/session.php";
---
+++ src/views/header.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-md bg-body-tertiary">
+            <div class="container-fluid">
+                
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="navbar-brand" href="index.php?r=index">
+                                <img src="images/logo.png" alt="Bootstrap" width="30" height="24">
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Fer una reserva</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Consultar reserves</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?r=login">Les meves dades</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?r=login">Iniciar sessió</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?r=register">Registrar</a>
+                        </li>
+                    </ul>
+                </div>
+
+                    </form>
+                    <button type="button" href="index.php?r=login" class="btn btn-primary">Iniciar sessió</button>
+                    <button type="button" class="btn btn-primary">Registrar-se</button>
+            </div>
+        </nav>
+
+
