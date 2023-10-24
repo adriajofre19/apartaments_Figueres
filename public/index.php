0f567ea24e4f02021b92bb84751d98011c73b9af
Marcos Muñoz <mmunoz2@cendrassos.net>
2023-10-24 17:16:52 +0200

    inici sessio base de dades

--- public/index.php
+++ public/index.php
@@ -1,38 +1,6 @@
 <?php
 
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
-<<<<<<< HEAD
-include "../src/config.php";
-
-include "../src/controllers/login.php";
-include "../src/controllers/index.php";
-include "../src/controllers/register.php";
-include "../src/controllers/rooms.php";
-
-$r = $_REQUEST["r"] ?? "";
-
-/* Creem els diferents models */
-$container = new Emeset\Container($config);
-$response = new Emeset\Response();
-$request = new Emeset\Request();
-
-if ($r === "login") {
-    $response = ctrlLogin($request, $response, $container);
-}
-elseif ($r === "index") {
-    $response = ctrlIndex($request, $response, $container);
-}
-elseif ($r === "register") {
-    $response = ctrlRegister($request, $response, $container);
-}
-elseif ($r === "rooms") {
-    $response = ctrlRooms($request, $response, $container);
-}
-else {
-    $response = ctrlIndex($request, $response, $container);
-}
-$response->response();
-=======
 
 include "../src/config.php";
 include "../src/controllers/index.php";
@@ -72,4 +40,3 @@ if($r == "") {
  }
 
   $response->response();
->>>>>>> d9a2fd5 (segon commit)
