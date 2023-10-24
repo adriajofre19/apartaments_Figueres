4bfe498fa2ad5cdbd9d7306126236b5ab0dbfacc
Adria Jofre Cantón <ajofre@cendrasso.net>
2023-10-24 16:48:25 +0200

    tercer

--- src/controllers/doLogout.php
+++ src/controllers/doLogout.php
@@ -5,6 +5,8 @@ function ctrlDoLogout($request, $response, $container){
     $response->setSession("user", []);
     $response->setSession("logged", false);
     $response->redirect("location: index.php");
+
+    echo("jofre");
     
     return $response;
 }
