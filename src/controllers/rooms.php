68262da04383bc3ae353f10cb60257d81ac4338d
Marcos Muñoz <mmunoz2@cendrassos.net>
2023-10-25 18:31:24 +0200

    habitacions fetes

--- src/controllers/rooms.php
+++ src/controllers/rooms.php
@@ -1,6 +1,15 @@
 <?php
 
-function ctrlRooms($request, $response, $container) {
+session_start();
+
+function CtrlRooms($request, $response, $container) {
+
+    $taskModel = $container->users();
+    $tasks = $taskModel->getRooms();
+
+    $response->set("tasks", $tasks);
+
     $response->setTemplate("rooms.php");
+    
     return $response;
 }
--- src/views/rooms.php
+++ src/views/rooms.php
@@ -1,27 +1,40 @@
-
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Habitación Moderna</title>
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <title>HOTELS</title>
 </head>
-
-<body class="body_apartaments">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+<body>
     <header>
-        <?php include 'header.php'; ?>
+        <?php include 'header.php' ?>
     </header>
-
-    <div class="container">
-        <div class="room-card">
-            <h1>Habitación Moderna</h1>
-            <p class="Info_habitacions">Esta habitación moderna cuenta con todas las comodidades que necesitas para una estancia relajante. Incluye una cama king-size, baño privado y vistas impresionantes.</p>
-            <p class="Preu_habitacions"><strong>Precio por noche:</strong> $150</p>
+<div class="apartaments">
+   <div class="row row-cols-1 row-cols-md-3 g-4">      
+        <?php foreach ($tasks as $task): ?>
+    <div class="col">
+        <div class="card h-100">
+            <img src="images/habitacio<?= $task['ID']; ?>.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"><?= $task['Titol']; ?></h5>
+                <p class="card-text"><?= $task['Preu_Alta']; ?></p>
+                <p class="card-text"><?= $task['Preu_Baixa']; ?></p>
+                <p class="card-text"><?= $task['Temporades']; ?></p>
+                <p class="card-text"><?= $task['Longitud']; ?></p>
+                <p class="card-text"><?= $task['Latitud']; ?></p>
+                <p class="card-text"><?= $task['N_Habitacions']; ?></p>
+                <p class="card-text"><?= $task['Metres_Quadrats']; ?></p>
+                <p class="card-text"><?= $task['Descripcio']; ?></p>
+                <p class="card-text"><?= $task['Adreca_Postal']; ?></p>
+            </div>
         </div>
     </div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d11810.164882281078!2d2.96756695!3d42.26696895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2ses!4v1698079438688!5m2!1ses!2ses" class="map" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <footer>
-<?php include 'footer.php' ?>
-</footer>
+<?php endforeach; ?>
+        
+        
+      </div>
+    </div>
+  </div>
+</div>
 </body>
+</html>
