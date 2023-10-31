04ad13273441c78d415e805675ad239650a53a74
Marcos Muñoz <mmunoz2@cendrassos.net>
2023-10-31 17:19:31 +0100

    d

--- src/views/header.php
+++ src/views/header.php
@@ -6,25 +6,29 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="navbar-brand" href="index.php?r=index">
-                                <img src="images/logo.png" alt="Bootstrap" width="30" height="24">
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php?r=rooms">Fer una reserva</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php?r=check_reserve">Consultar reserves</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php?r=add_apps">Afegir apartaments</a>
-                        </li>
-                    </ul>
-                </div>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="navbar-brand" href="index.php?r=index">
+                        <img src="images/logo.png" alt="Bootstrap" width="30" height="30">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Fer una reserva</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Consultar reserves</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?r=rooms">Apartaments</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?r=add_apps">Afegir un apartament</a>
+                </li>
+            </ul>
+        </div>
 
+<<<<<<< HEAD
                     </form>
                     <div class="dropdown dropstart">
   <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -38,4 +42,30 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
     crossorigin="anonymous"></script>
+=======
+        <?php if (isset($_SESSION['user']['user'])) { ?>
+            <div class="dropdown dropstart">
+                <button class="button_" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="images/inici.png" alt="" width="30" height="30">
+                </button>
+                <ul class="dropdown-menu dropdown-menu-dark">
+                    <li><a class="dropdown-item"><?php echo $_SESSION['user']['user']; ?></a></li>
+                    <li><a class="dropdown-item" href="index.php?r=dades">Les meves dades</a></li>
+                    <li><a class="dropdown-item" href="index.php?r=dologout">Tancar sessió</a></li>
+                </ul>
+            </div>
+        <?php } else { ?>
+            <div class="dropdown dropstart">
+                <button class="button_" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="images/inici.png" alt="" width="30" height="30">
+                </button>
+                <ul class="dropdown-menu dropdown-menu-dark">
+                    <li><a class="dropdown-item" href="index.php?r=login">Iniciar sessió</a></li>
+                    <li><a class="dropdown-item" href="index.php?r=register">Registrar</a></li>
+                </ul>
+            </div>
+        <?php } ?>
+    </div>
+</nav>
+>>>>>>> 2342770 (eliminarApartaments)
 
