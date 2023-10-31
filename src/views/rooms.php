6b107646f346bba1a1cd689468e4fe9c6e849ab3
Marcos Muñoz <mmunoz2@cendrassos.net>
2023-10-31 19:46:17 +0100

    info apartaments base de dades

--- src/views/rooms.php
+++ src/views/rooms.php
@@ -12,7 +12,6 @@
 <body>
     <header>
         <?php include 'header.php'; ?>
-        <?php include 'header.php'; ?>
     </header>
     <div class="apartments">
         <div class="row row-cols-1 row-cols-md-3 g-4">
@@ -38,6 +37,9 @@
 </body>
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
 <script>
     <?php foreach ($apps as $app): ?>
         var mymap<?= $app['ID']; ?> = L.map('map<?= $app['ID']; ?>-modal').setView([<?= $app['Latitud']; ?>, <?= $app['Longitud']; ?>], 25);
