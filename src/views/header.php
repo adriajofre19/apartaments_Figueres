74a815114c4302c2489e97d7e37f39df14a1d93f
Marcos Muñoz <mmunoz2@cendrassos.net>
2023-10-31 19:29:17 +0100

    info habitacions index

--- src/views/header.php
+++ src/views/header.php
@@ -28,21 +28,6 @@
             </ul>
         </div>
 
-<<<<<<< HEAD
-                    </form>
-                    <div class="dropdown dropstart">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-  <?=$user['user'];?>
-  </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="index.php?r=dades">Les meves Dades</a></li>
-    <li><a class="dropdown-item" href="index.php?r=doLogout">Tancar Sessió(<?=$user['user'];?>)</a></li>
-  </ul>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-    crossorigin="anonymous"></script>
-=======
         <?php if (isset($_SESSION['user']['user'])) { ?>
             <div class="dropdown dropstart">
                 <button class="button_" type="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -67,5 +52,4 @@
         <?php } ?>
     </div>
 </nav>
->>>>>>> 2342770 (eliminarApartaments)
 
