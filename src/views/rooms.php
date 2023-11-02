40e84b6d012ec4eaeeb72a4b3f7dc9f2702cd525
Marcos Muñoz <mmunoz2@cendrassos.net>
2023-11-02 18:40:00 +0100

    80% modal

--- src/views/rooms.php
+++ src/views/rooms.php
@@ -14,24 +14,44 @@
         <?php include 'header.php'; ?>
     </header>
     <div class="apartments">
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            <?php foreach ($apps as $app): ?>
-                <div class="col">
-                    <div class="card h-100">
-                        <img src="images/habitacio<?= $app['ID']; ?>.jpg" class="card-img-top" alt="..." data-bs-toggle="modal" data-bs-target="#hotelModal<?= $app['ID']; ?>">
-                        <div class="card-body">
-                            <h5 class="card-title"><?= $app['Titol']; ?></h5>
-                            <p class="card-text">Preu: <?= $app['Preu_Alta']; ?></p>
-                            <p class="card-text">Habitacions: <?= $app['N_Habitacions']; ?></p>
-                            <p class="card-text">Adreça: <?= $app['Adreca_Postal']; ?></p>
-                            <div id="map<?= $app['ID']; ?>-modal" style="height: 400px;"></div>
-                            <a href="index.php?r=delete&id=<?= $app['ID']; ?>" class="btn btn-danger">Eliminar</a>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <?php foreach ($apps as $app): ?>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="images/habitacio<?= $app['ID']; ?>.jpg" class="card-img-top" alt="..."
+                        data-bs-toggle="modal" data-bs-target="#hotelModal<?= $app['ID']; ?>">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $app['Titol']; ?></h5>
+                        <p class="card-text">Preu: <?= $app['Preu_Alta']; ?></p>
+                        <p class="card-text">Habitacions: <?= $app['N_Habitacions']; ?></p>
+                        <p class="card-text">Adreça: <?= $app['Adreca_Postal']; ?></p>
+                        <div class="modal fade" id="hotelModal<?= $app['ID']; ?>" tabindex="-1" aria-labelledby="hotelModalLabel<?= $app['ID']; ?>" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="hotelModalLabel<?= $app['ID']; ?>">Información del Apartamento</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                    <h5><?= $app['Titol']; ?></h5>
+                                    <p>Precio: $<?= $app['Preu_Alta']; ?></p>
+                                    <img src="images/habitacio<?= $app['ID']; ?>.jpg" alt="<?= $app['Titol']; ?>" class="img-fluid">
+                                    </div>
+                                    <div class="modal-footer">
+                                        <a href="index.php?r=delete&id=<?= $app['ID']; ?>" class="btn btn-danger">Eliminar</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        <!-- Fin del modal -->
                     </div>
+                    <div id="map<?= $app['ID']; ?>-modal" style="height: 300px;"></div>
                 </div>
-            <?php endforeach; ?>
-        </div>
+            </div>
+        <?php endforeach; ?>
     </div>
+</div>
+
 
     
 </body>
